6d4af9318fad3aa83b7f9b3310cadca5abdc840b
abduyus <yusuf.abdurrasheed@outlook.com>
2025-07-27 13:34:50 +0500

    Getting API call with useEffect

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 const tempMovieData = [
   {
@@ -50,94 +50,94 @@ const tempWatchedData = [
 const average = (arr) =>
   arr.reduce((acc, cur, i, arr) => acc + cur / arr.length, 0);
 
+const KEY = "970b89c2";
 
 export default function App() {
-    const [movies, setMovies] = useState(tempMovieData);
-    const [watched, setWatched] = useState(tempWatchedData);
-    return (
-        <>
-            <NavBar>
-                <Search/>
-                <NumResults movies={movies}/>
-            </NavBar>
+  const [movies, setMovies] = useState([]);
+  const [watched, setWatched] = useState([]);
 
+  useEffect(function () {
+    fetch(`http://www.omdbapi.com/?apikey=${KEY}&s=iron man`)
+      .then((res) => res.json())
+      .then((data) => setMovies(data.Search));
+  }, []);
 
-            <Main>
-                <Box>
-                    <MovieList movies={movies}/>
-                </Box>
+  return (
+    <>
+      <NavBar>
+        <Search />
+        <NumResults movies={movies} />
+      </NavBar>
 
-                <Box>
-                    <WatchedSummary watched={watched} />
-                    <WatchedMovieList watched={watched} />
-                </Box>
-            </Main>
+      <Main>
+        <Box>
+          <MovieList movies={movies} />
+        </Box>
 
-        </>
-    );
+        <Box>
+          <WatchedSummary watched={watched} />
+          <WatchedMovieList watched={watched} />
+        </Box>
+      </Main>
+    </>
+  );
 }
 
-
 function NavBar({ children }) {
-    return (
-        <nav className="nav-bar">
-            <Logo/>
-            {children}
-        </nav>)
+  return (
+    <nav className="nav-bar">
+      <Logo />
+      {children}
+    </nav>
+  );
 }
 
 function Logo() {
-    return (
-        <div className="logo">
-            <span role="img">🍿</span>
-            <h1>usePopcorn</h1>
-        </div>
-    )
+  return (
+    <div className="logo">
+      <span role="img">🍿</span>
+      <h1>usePopcorn</h1>
+    </div>
+  );
 }
 
 function Search() {
-    const [query, setQuery] = useState("");
-    return (
-        <input
-            className="search"
-            type="text"
-            placeholder="Search movies..."
-            value={query}
-            onChange={(e) => setQuery(e.target.value)}
-        />
-    )
+  const [query, setQuery] = useState("");
+  return (
+    <input
+      className="search"
+      type="text"
+      placeholder="Search movies..."
+      value={query}
+      onChange={(e) => setQuery(e.target.value)}
+    />
+  );
 }
 
-function NumResults({movies}) {
-    return (
-        <p className="num-results">
-            Found <strong>{movies.length}</strong> results
-        </p>
-    )
+function NumResults({ movies }) {
+  return (
+    <p className="num-results">
+      Found <strong>{movies.length}</strong> results
+    </p>
+  );
 }
 
-function Main({children}) {
-    return (
-        <main className="main">
-            {children}
-        </main>
-    )
+function Main({ children }) {
+  return <main className="main">{children}</main>;
 }
 
-function Box({children}) {
-    const [isOpen, setIsOpen] = useState(true);
-    return (
-        <div className="box">
-            <button
-                className="btn-toggle"
-                onClick={() => setIsOpen((open) => !open)}
-            >
-                {isOpen ? "–" : "+"}
-            </button>
-            {isOpen && children}
-        </div>
-    )
+function Box({ children }) {
+  const [isOpen, setIsOpen] = useState(true);
+  return (
+    <div className="box">
+      <button className="btn-toggle" onClick={() => setIsOpen((open) => !open)}>
+        {isOpen ? "–" : "+"}
+      </button>
+      {isOpen && children}
+    </div>
+  );
 }
+
 //
 // function WatchedBox() {
 
@@ -164,83 +164,79 @@ function Box({children}) {
 //     )
 // }
 
-function MovieList({movies}) {
-    return (
-        <ul className="list">
-            {movies?.map((movie) => (
-                <Movie movie={movie} key={movie.imdbID}>
-                    <p>
-                        <span>🗓</span>
-                        <span>{movie.Year}</span>
-                    </p>
-                </Movie>
-            ))}
-        </ul>
-            )
+function MovieList({ movies }) {
+  return (
+    <ul className="list">
+      {movies?.map((movie) => (
+        <Movie movie={movie} key={movie.imdbID}>
+          <p>
+            <span>🗓</span>
+            <span>{movie.Year}</span>
+          </p>
+        </Movie>
+      ))}
+    </ul>
+  );
 }
 
-function Movie({movie, children}) {
-    return (
-        <li >
-            <img src={movie.Poster} alt={`${movie.Title} poster`}/>
-            <h3>{movie.Title}</h3>
-            <div>
-                {children}
-            </div>
-        </li>
-    )
+function Movie({ movie, children }) {
+  return (
+    <li>
+      <img src={movie.Poster} alt={`${movie.Title} poster`} />
+      <h3>{movie.Title}</h3>
+      <div>{children}</div>
+    </li>
+  );
 }
 
-
-
-function WatchedSummary({watched}) {
-    const avgImdbRating = average(watched.map((movie) => movie.imdbRating));
-    const avgUserRating = average(watched.map((movie) => movie.userRating));
-    const avgRuntime = average(watched.map((movie) => movie.runtime));
-    return (
-        <div className="summary">
-            <h2>Movies you watched</h2>
-            <div>
-                <p>
-                    <span>#️⃣</span>
-                    <span>{watched.length} movies</span>
-                </p>
-                <p>
-                    <span>⭐️</span>
-                    <span>{avgImdbRating}</span>
-                </p>
-                <p>
-                    <span>🌟</span>
-                    <span>{avgUserRating}</span>
-                </p>
-                <p>
-                    <span>⏳</span>
-                    <span>{avgRuntime} min</span>
-                </p>
-            </div>
-        </div>
-    )
+function WatchedSummary({ watched }) {
+  const avgImdbRating = average(watched.map((movie) => movie.imdbRating));
+  const avgUserRating = average(watched.map((movie) => movie.userRating));
+  const avgRuntime = average(watched.map((movie) => movie.runtime));
+  return (
+    <div className="summary">
+      <h2>Movies you watched</h2>
+      <div>
+        <p>
+          <span>#️⃣</span>
+          <span>{watched.length} movies</span>
+        </p>
+        <p>
+          <span>⭐️</span>
+          <span>{avgImdbRating}</span>
+        </p>
+        <p>
+          <span>🌟</span>
+          <span>{avgUserRating}</span>
+        </p>
+        <p>
+          <span>⏳</span>
+          <span>{avgRuntime} min</span>
+        </p>
+      </div>
+    </div>
+  );
 }
 
-function WatchedMovieList({watched}) {
-    return (
-        <ul className="list">
-            {watched.map((movie) => (
-                <Movie movie={movie} key={movie.imdbID}>
-                    <p>
-                        <span>⭐️</span>
-                        <span>{movie.imdbRating}</span>
-                    </p>
-                    <p>
-                        <span>🌟</span>
-                        <span>{movie.userRating}</span>
-                    </p>
-                    <p>
-                        <span>⏳</span>
-                        <span>{movie.runtime} min</span>
-                    </p>
-                </Movie>
-            ))}
-        </ul>
-    )
+function WatchedMovieList({ watched }) {
+  return (
+    <ul className="list">
+      {watched.map((movie) => (
+        <Movie movie={movie} key={movie.imdbID}>
+          <p>
+            <span>⭐️</span>
+            <span>{movie.imdbRating}</span>
+          </p>
+          <p>
+            <span>🌟</span>
+            <span>{movie.userRating}</span>
+          </p>
+          <p>
+            <span>⏳</span>
+            <span>{movie.runtime} min</span>
+          </p>
+        </Movie>
+      ))}
+    </ul>
+  );
 }
